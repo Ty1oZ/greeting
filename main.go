00b23469501e2af176588730afd7519e10d5a8dd
Ty1oZ <167120037+Ty1oZ@users.noreply.github.com>
2024-07-10 14:42:15 +0300

    a new version program average2 (v2)

--- main.go
+++ main.go
@@ -9,16 +9,24 @@ import (
 	"strconv"
 )
 
+func average(numbers ...float64) float64 {
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum / float64(len(numbers))
+}
+
 func main() {
 	arguments := os.Args[1:]
-	var sum float64 = 0                    // Объявление переменной для хранения суммы чисел
+	var numbers []float64                  // В этом сегменте будут храниться числа, для которых вычисляется среднее
 	for _, arargument := range arguments { // обрабатываем каждый элемент командной строки
 		number, err := strconv.ParseFloat(arargument, 64) // строка преобразуется в float64
 		if err != nil {                                   // если при преобразовании строки произошла ошибка, программа выводит сообщение и завершается
 			log.Fatal(err)
 		}
-		sum += number // число прибавляется к сумме
+		numbers = append(numbers, number)
+
 	}
-	sampleCount := float64(len(arguments))          // длина сегмента argemnts используется как количество значений данных
-	fmt.Printf("Average: %0.2f\n", sum/sampleCount) // вычесление среднего значения и его вывод
+	fmt.Printf("Average: %0.2f\n", average(numbers...)) // вычесление среднего значения и его вывод
 }
